90cc5097da1cc56d30d8cdda43cfdeda859c5bff
chinnadas <chinnadas>
2019-06-27 15:14:09 +0530

    added Terraform-gol

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    region = "us-west-2"
+    access_key = "AKIAWLW3NYRIFYVJLSHU"
+    secret_key = "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"
+}
+
+resource "aws_instance" "gameoflife" {
+   ami = "ami-05b598e8868af8430"
+   key_name = "Terraform"
+   instance_type = "t2.micro"
+   vpc_security_group_ids= ["sg-049ae073"]
+
+   connection {
+       type = "ssh"
+       user = "ubuntu"
+       private_key = "${file("Terraform.pem")}"
+   }
+
+   provisioner "remote-exec" {
+       inline = ["git clone https://github.com/chinnadas/gol-playbook.git","ansible-playbook gol-playbook/gol.yml -i hosts"]
+   } 
+}
